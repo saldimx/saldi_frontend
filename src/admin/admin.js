d0867f27a5c39f510e4089aef956836ef0d0f13e
Obed Vega <vega.obed@gmail.com>
2022-05-20 17:47:15 -0700

    fixing deploy

--- src/admin/admin.js
+++ src/admin/admin.js
@@ -74,9 +74,7 @@ export const Admin = () => {
 
         <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
           <br/>
-          <Outlet/>
-        
-      
+          <Outlet/>    
         </main>
       </div>
     </div>
