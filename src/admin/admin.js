38175cb921ab4236189cac1f9cddf289982de21a
Obed Vega <vega.obed@gmail.com>
2022-05-20 17:59:08 -0700

    deployment

--- src/admin/admin.js
+++ src/admin/admin.js
@@ -15,10 +15,22 @@ const ayuda = <FontAwesomeIcon icon={faQuestion} />
 export const Admin = () => {
     return(
     <>
- <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+    <header 
+      className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
         <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#s">Saldi</a>
-        <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"></input>
-        <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <input 
+          className="form-control form-control-dark w-100" 
+          type="text" 
+          placeholder="Search" 
+          aria-label="Search">
+        </input>
+        <button 
+          className="navbar-toggler position-absolute d-md-none collapsed" 
+          type="button" data-bs-toggle="collapse" 
+          data-bs-target="#sidebarMenu" 
+          aria-controls="sidebarMenu" 
+          aria-expanded="false" 
+          aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="navbar-nav">
@@ -29,7 +41,9 @@ export const Admin = () => {
       </header>
       <div className="container-fluid">
       <div className="row">
-        <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse" >
+        <nav 
+          id="sidebarMenu" 
+          className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse" >
           <div className="position-sticky pt-3">
             <ul className="nav flex-column">
               <li className="nav-item">
